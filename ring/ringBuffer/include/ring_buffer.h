fee2d4be950b132b5d5a694525af41690cb4569e
Anthony Agby <anthonyagby@Anthonys-MacBook-Pro.local>
2026-03-17 16:53:05 +0100

    Add ring buffer module with header, source, main, and Makefile

---
+++ ring/ringBuffer/include/ring_buffer.h
@@ -0,0 +1,21 @@
+#include <stdint.h>
+
+typedef struct {
+    uint8_t * const buffer;
+    int head;
+    int tail;
+    const int maxlen;
+} circ_bbuf_t;
+
+int circ_bbuf_push(circ_bbuf_t *c, uint8_t data);
+int circ_bbuf_pop(circ_bbuf_t *c, uint8_t *data);
+
+//setting up the buffer definition
+#define CIRC_BBUF_DEF(x,y)                \
+    uint8_t x##_data_space[y];            \
+    circ_bbuf_t x = {                     \
+        .buffer = x##_data_space,         \
+        .head = 0,                        \
+        .tail = 0,                        \
+        .maxlen = y                       \
+    }
