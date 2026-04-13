6dfa1cafb145867624718894a182616a87994d58
Anthony J. Agby Jr <agbyaj@gmail.com>
2026-04-13 17:12:29 +0200

    Added scaffolding for streaming JSON parser/tokenizer.

---
+++ json_parser/include/json_stream_parser.h
@@ -0,0 +1,8 @@
+#ifndef JSON_STREAM_PARSER_H
+#define JSON_STREAM_PARSER_H
+
+typedef struct json_stream_parser json_stream_parser_t;
+
+void json_stream_parser_init(json_stream_parser_t *p);
+
+#endif
---
+++ json_parser/include/json_stream_tokenizer.h
@@ -0,0 +1,8 @@
+#ifndef JSON_STREAM_TOKENIZER_H
+#define JSON_STREAM_TOKENIZER_H
+
+typedef struct json_stream_tokenizer json_stream_tokenizer_t;
+
+void json_stream_tokenizer_init(json_stream_tokenizer_t *t);
+
+#endif
