c9c1762ad1fa8b146c58df60abc37eb59c0e3f7a
Anthony J. Agby Jr <agbyaj@gmail.com>
2026-04-28 17:05:38 +0200

    json writer enums and struct setup.

--- json_writer/src/json_writer.c
+++ json_writer/src/json_writer.c
@@ -1,1 +1,40 @@
 #include "json_writer.h"
+#include <stddef.h>
+
+#define JSON_WRITER_MAX_DEPTH 32
+
+//internal scope
+typedef enum {
+    JW_SCOPE_NONE = 0,
+    JW_SCOPE_OBJECT,
+    JW_SCOPE_ARRAY
+} jw_scope_t;
+
+//state machine
+typedef enum {
+    JW_STATE_START = 0,     // nothing written
+    JW_STATE_KEY,           // expecting a key (inside object)
+    JW_STATE_VALUE,         // expecting a value (after key or inside array)
+    JW_STATE_AFTER_VALUE    // value written (expect comma or close object/array)
+} jw_state_t;
+
+struct json_writer {
+    // Output plumbing
+    json_writer_output_fn out;
+    void *ctx;
+
+    // Formatting configuration
+    int pretty;
+    int indent_width;
+    int indent_level;
+
+    // State machine
+    jw_state_t state;
+
+    // Scope stack for nested objects/arrays
+    jw_scope_t scope_stack[JSON_WRITER_MAX_DEPTH];
+    int depth;
+
+    // Whether the next value should be preceded by a comma
+    int need_comma;
+};
